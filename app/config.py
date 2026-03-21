54317d0c78b07851cc097ae1dcf1713c67b6a266
Dmitry Tmks <tmks.dm@gmail.com>
2026-03-21 21:15:30 +1000

    Синхронизация с сервера

--- app/config.py
+++ app/config.py
@@ -13,7 +13,7 @@ SESSION_NAME = 'news_aggregator_session'
 # AI
 OPENROUTER_API_KEY = os.getenv('OPENROUTER_API_KEY')
 # Модель, которую ты выбрал
-AI_MODEL = "xiaomi/mimo-v2-flash:free"
+AI_MODEL = "arcee-ai/trinity-large-preview:free"
 
 # === Канал для технических отчетов ===
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
